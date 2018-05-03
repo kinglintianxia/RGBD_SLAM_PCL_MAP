63a694a8e122434a1b45483d13774d4b89415eaa
kinglintianxia <wangliujun_bit@163.com>
2018-05-03 14:50:57 +0800

    add fr1_room pcl

---
+++ fr1_room.sh
@@ -0,0 +1,1 @@
+./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.bin Examples/RGB-D/TUM1.yaml /home/king/Documents/image/rgbd_dataset_freiburg1_room /home/king/Documents/image/rgbd_dataset_freiburg1_room/associations.txt /home/king/Documents/king/ORB_SLAM2_MAP/map/fr1_room.bin false
